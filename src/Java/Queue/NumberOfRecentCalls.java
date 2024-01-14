5bb2786e9c166859208e6f3cbb51cd1292ae20ea
KahoMoon <kahomoon1@gmail.com>
2024-01-14 15:11:31 -0800

    Passed all testcases.

---
+++ src/Java/Queue/NumberOfRecentCalls.java
@@ -0,0 +1,33 @@
+package Java.Queue;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class NumberOfRecentCalls {
+    public static void main(String[] args) {
+        RecentCounter test = new RecentCounter();
+        System.out.println(test.ping(1));
+        System.out.println(test.ping(100));
+        System.out.println(test.ping(3001));
+        System.out.println(test.ping(3002));
+    }
+
+    static class RecentCounter {
+
+        Deque<Integer> counter;
+        public RecentCounter() {
+            counter = new LinkedList<>();
+        }
+
+        public int ping(int t) {
+            while (!counter.isEmpty() && t - counter.getFirst() > 3000) {
+                counter.removeFirst();
+            }
+            counter.add(t);
+
+            return counter.size();
+        }
+
+    }
+
+}
