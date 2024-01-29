349d9f4ff4af8ec8b6d38a24d0d3606a0c7d3b1e
KahoMoon <kahomoon1@gmail.com>
2024-01-29 15:35:31 -0800

    Passed all testcases.

---
+++ src/Java/LinkedList/MaximumTwinSumOfALinkedList.java
@@ -0,0 +1,34 @@
+package Java.LinkedList;
+
+import Java.ListNode;
+
+import java.util.ArrayList;
+
+public class MaximumTwinSumOfALinkedList {
+    public static void main(String[] args) {
+        ListNode third = new ListNode(1);
+        ListNode second = new ListNode(2, third);
+        ListNode first = new ListNode(4, second);
+        ListNode zero = new ListNode(5, first);
+
+        pairSum(zero);
+    }
+
+    public static int pairSum(ListNode head) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        while (head != null) {
+            arr.add(head.val);
+            head = head.next;
+        }
+
+        int max = -999;
+        for (int i = arr.size() / 2; i < arr.size(); i++) {
+            int curr = arr.get(i);
+            int twin = arr.get(arr.size() - 1 - i);
+
+            max = Math.max(max, curr + twin);
+        }
+
+        return max;
+    }
+}
